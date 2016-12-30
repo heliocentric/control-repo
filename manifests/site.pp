96aea73a97ac1efb04c216f003f1876e783a8b6c
Dylan Cochran <dylan.cochran@onyxpoint.com>
2016-12-30 13:44:44 -0500

    Fix default

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,6 +33,6 @@ node mom2.test {
   #  include $hiera_include_classes
 }
 $simp_options_puppet_server_distribution = simplib::lookup("simp_options::puppet::server_distribution")
-$test = simplib::lookup("test", undef, undef, "blah")
+$test = simplib::lookup("test", { default_value => 'blah'})
 notify { "site.pp - ${test}": }
 notify { "puppet master serverversion: $::serverversion": }
